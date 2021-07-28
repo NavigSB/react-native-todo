df446f0fe3b707557debcb70708e8b107c965b36
navigsb <navigsb@uw.edu>
2021-07-28 11:35:26 -0700

    Added header

--- App.js
+++ App.js
@@ -1,21 +1,31 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React from "react";
+import { StyleSheet, Text, View } from "react-native";
+import DrawerNavigator from "./src/Navigation/DrawerNavigator";
+import TodoItem from "./src/Todo/TodoItem";
+
+function ListScreen() {
+  return (
+    <View style={styles.container}>
+      <TodoItem>
+        <Text>First item!</Text>
+      </TodoItem>
+    </View>
+  );
+}
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <DrawerNavigator screenOptions={{headerShown: true}}>
+      <ListScreen name="List 1" title="List 1" />
+    </DrawerNavigator>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
